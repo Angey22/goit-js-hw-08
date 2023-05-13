de45a2b769b2933fff985539d3a777808ad1c182
Angey22 <babandval@gmail.com>
2023-05-13 17:19:54 +0300

    Added code of the fierst task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,36 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// Connected the library "SimpleLightbox"
+import SimpleLightbox from "simplelightbox";
+
+// Connected CSS styles of the library "SimpleLightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Link on the <ul> element of HTML document
+const galleryListEl = document.querySelector('.gallery');
+
+// Generate markup
+const MakeGalleryMarkup = items => {
+    return items.map(({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+            />
+        </a>
+    </li>`
+    ).join('');
+}
+
+// Add markup in DOM
+galleryListEl.innerHTML = MakeGalleryMarkup(galleryItems);
+
+// Set library "SimpleLightbox"
+new SimpleLightbox('.gallery__link', {
+    sourceAttr: 'href',
+    captionsData: 'alt',
+    captionDelay: 250,
+ });
